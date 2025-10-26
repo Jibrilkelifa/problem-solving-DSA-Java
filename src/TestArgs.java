df90133a087bae2aa57301bce17006f9de413e88
Jibril Kalifa <120446317+Jibrilkelifa@users.noreply.github.com>
2025-10-26 16:58:16 +0300

    ArraylIST

---
+++ src/TestArgs.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class TestArgs {
+    public static void main(String[] args) {
+        System.out.println("Number of arguments: " + args.length);
+        for (String arg : args) {
+            System.out.println("Arg: " + arg);
+        }
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Jibril");
+        names.add("kunal");
+
+        for ( int i=0; i < names.size(); i++){
+            System.out.println(names.get(i));
+        }
+    }
+
+}
+
